78dbd4bd28359ec9f1d40afe233adcc127c93de2
Eduardo Flores <eduardo@desktop.ipv6.infinitum.net.mx>
2023-12-19 09:14:44 -0600

    fix script

--- scripts/set_theme.sh
+++ scripts/set_theme.sh
@@ -1,9 +1,9 @@
 # Pick a theme
 current_hour=$(date +%H)
 case $current_hour in
-    8|9|10|11|12|13) theme="gruvbox" ;;
+    08|09|10|11|12|13) theme="gruvbox" ;;
     14|15|16|17|18|19) theme="everforest" ;;
-    20|21|22|23|0|1|2|3|4|5|6|7) theme="dracula" ;;
+    20|21|22|23|00|01|02|03|04|05|06|07) theme="dracula" ;;
 esac
 
 # For testing purposes
