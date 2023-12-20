b724af66682122e21be9807e5dffeefaebafcb37
edu-flores <eduardo.floresramones@gmail.com>
2023-12-19 19:37:15 -0600

    increase dark hours

--- config/i3/scripts/set_theme.sh
+++ config/i3/scripts/set_theme.sh
@@ -3,9 +3,9 @@
 # Pick a theme
 current_hour=$(date +%H)
 case $current_hour in
-    08|09|10|11|12|13) theme="gruvbox" ;;
-    14|15|16|17|18|19) theme="everforest" ;;
-    20|21|22|23|00|01|02|03|04|05|06|07) theme="dracula" ;;
+    07|08|09|10|11|12|13) theme="gruvbox" ;;
+    14|15|16|17|18) theme="everforest" ;;
+    19|20|21|22|23|00|01|02|03|04|05|06) theme="dracula" ;;
 esac
 
 # For testing purposes
