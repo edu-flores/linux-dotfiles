aaa0c9cd3763406f351a21df922a0db7f622d1d0
edu-flores <eduardo.floresramones@gmail.com>
2023-12-23 13:51:03 -0600

    calendar module

---
+++ config/polybar/scripts/display_calendar.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# File to store the month difference
+tmp_file="/tmp/calendar_month_diff"
+
+# Determine month difference based on the argument
+case $1 in
+  "next")
+    month_diff=$(($(cat $tmp_file 2>/dev/null) + 1))
+    ;;
+  "prev")
+    month_diff=$(($(cat $tmp_file 2>/dev/null) - 1))
+    ;;
+  *)
+    month_diff=0
+    ;;
+esac
+
+# Store the month difference for next/prev
+echo "$month_diff" > $tmp_file
+
+# Get the calendar for the specified month
+if [ $month_diff -eq 0 ]; then
+    calendar=$(cal | awk -v today="$(date "+%e")" \
+    '{gsub(" " today " ", " <u><b>" today "</b></u> "); print}')  # Highlight today
+else
+    calendar=$(cal $(date -d "$month_diff months" "+%m %Y"))
+fi
+
+# Show notification
+HEAD=$(echo "$calendar" | head -n 1)
+BODY=$(echo "$calendar" | tail -n +2)
+FOOT="\n  Calendar"
+dunstify "$HEAD" "$BODY$FOOT" -r -1
