9ca3a20af58d3101d11758abde4200562b080f6a
edu-flores <eduardo.floresramones@gmail.com>
2024-08-16 22:25:44 -0600

    redesign power menu

--- config/polybar/scripts/open_power_menu.sh
+++ config/polybar/scripts/open_power_menu.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 # Define power menu options
-options="  POWER\n  REBOOT\n󰒲  SLEEP"
+options="\n\n󰒲\n"
 
 # Display Rofi menu and store the selected option
 selected_option=$(echo -e "$options" | rofi -dmenu -i -p "Power Menu" -theme-str '@import "power.rasi"')
 
 # Perform actions based on the selected option
 case "$selected_option" in
-    "  POWER")
+    "")
         systemctl poweroff
         ;;
-    "  REBOOT")
+    "")
         systemctl reboot
         ;;
-    "󰒲  SLEEP")
+    "󰒲")
         systemctl suspend
+        ;;
+    "")
+        systemctl hibernate
         ;;
     *)
         echo "Dismissed"
