804e9c0e13fdcb75bb93aa83342c43a940cecf09
Eduardo Flores <eduardo.floresramones@gmail.com>
2024-10-26 10:40:03 -0600

    use variables for monitors

--- config/i3/scripts/configure_display_settings.sh
+++ config/i3/scripts/configure_display_settings.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-xrandr --output DP-1 --rotate left
-xrandr --output DP-1 --left-of HDMI-1
+# Get displays
+PRIMARY_MONITOR=$1
+SECONDARY_MONITOR=$2
+
+# Dual monitor setup
+if [ -n "$SECONDARY_MONITOR" ] && [ -n "$PRIMARY_MONITOR" ]; then
+    xrandr --output "$SECONDARY_MONITOR" --rotate left
+    xrandr --output "$SECONDARY_MONITOR" --left-of "$PRIMARY_MONITOR"
+fi
