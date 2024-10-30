c214e505b242508be8c56be690ccfc5adb5b85e1
Eduardo Flores <eduardo.floresramones@gmail.com>
2024-10-29 18:49:20 -0600

    switch flameshot env var

--- config/i3/scripts/take_flameshot_screenshot.sh
+++ config/i3/scripts/take_flameshot_screenshot.sh
@@ -4,7 +4,7 @@
 focused_window=$(xdotool getactivewindow)
 
 # Take screenshot
-XDG_CURRENT_DESKTOP=GNOME flameshot gui > /dev/null
+XDG_CURRENT_DESKTOP=X-Cinnamon flameshot gui > /dev/null
 
 # Restore window
 [ "$focused_window" == "$(xdotool getactivewindow)" ] && xdotool windowfocus $focused_window
