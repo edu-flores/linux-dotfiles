810a11f7df929c2bde86a0dbca1bf748ac649ce5
edu-flores <eduardo.floresramones@gmail.com>
2024-12-20 18:39:40 -0600

    design new polybar

--- config/polybar/scripts/launch_polybar_program.sh
+++ config/polybar/scripts/launch_polybar_program.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Terminate already running bar instances
-pkill -f "polybar top"
+# pkill -f "polybar top"
+killall polybar
 
 # Launch
-polybar top &
+# polybar top &
+polybar -c ~/.config/polybar/polybar.ini main &
