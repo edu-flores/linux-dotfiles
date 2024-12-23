4c90c46a92d91b97301ff4c4a7a411968bc55420
edu-flores <eduardo.floresramones@gmail.com>
2024-12-23 17:15:35 -0600

    new power menu design

--- config/polybar/scripts/open_power_menu.sh
+++ config/polybar/scripts/open_power_menu.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
 # Define power menu options
-options="\n\n󰒲\n"
+options="⏼\n\n\n\n\n󰗽"
 
 # Display Rofi menu and store the selected option
-selected_option=$(echo -e "$options" | rofi -dmenu -i -p "Power Menu" -theme-str '@import "power.rasi"')
+uptime=$(uptime -p | sed 's/up //')
+selected_option=$(echo -e "$options" | rofi -dmenu -config ~/.config/rofi/power.rasi -mesg "System Uptime: $uptime")
 
 # Perform actions based on the selected option
 case "$selected_option" in
-    "")
+    "⏼")
         systemctl poweroff
         ;;
     "")
         systemctl reboot
         ;;
-    "󰒲")
+    "")
         systemctl suspend
         ;;
     "")
         systemctl hibernate
+        ;;
+    "")
+        echo "Locked"
+        ;;
+    "󰗽")
+        i3-msg exit
         ;;
     *)
         echo "Dismissed"
