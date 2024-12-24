b8211b974177840556ce5d59f23c1c540c7dfd8f
edu-flores <eduardo.floresramones@gmail.com>
2024-12-24 17:06:34 -0600

    new accent color

--- config/polybar/scripts/display_calendar_notification.sh
+++ config/polybar/scripts/display_calendar_notification.sh
@@ -22,7 +22,7 @@ echo "$month_diff" > $tmp_file
 # Get the calendar for the specified month
 if [ $month_diff -eq 0 ]; then
     today=$(date "+%e")
-    calendar=$(cal | sed "1n; 0,/$today/{s/$today/<span foreground='#fda234'>&<\/span>/}")  # Highlight today
+    calendar=$(cal | sed "1n; 0,/$today/{s/$today/<span foreground='#ff9f00'>&<\/span>/}")  # Highlight today
 else
     calendar=$(cal $(date -d "$(date +%Y-%m-01) $month_diff months" "+%m %Y"))
 fi
