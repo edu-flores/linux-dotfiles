490b87cb2458da6b33347c3723ed5ae723b73a6f
edu-flores <eduardo.floresramones@gmail.com>
2024-12-26 19:58:05 -0600

    add light and dark themes

--- config/i3/scripts/set_desktop_theme.sh
+++ config/i3/scripts/set_desktop_theme.sh
@@ -1,47 +1,29 @@
 #!/bin/bash
 
-# List of available themes
-themes=("everforest" "gruvbox" "tokyo" "nord")
-
-# Pick a random theme if $1 is not passed
-if [ -z "$1" ]; then
-    theme=${themes[$RANDOM % ${#themes[@]}]}
+# Toggle dark and light if no specific theme is passed
+if [[ -z "$1" || "$1" = "toggle" ]]; then
+    current_theme=$(awk '/@theme/ {print $2}' ~/.config/rofi/rofi.rasi | tr -d '"')
+    [ "$current_theme" = "dark" ] && theme="light" || theme="dark"
 else
     # Select a theme based on the user's choice
     case $1 in
-        1) theme="everforest" ;;
-        2) theme="gruvbox" ;;
-        3) theme="tokyo" ;;
-        4) theme="nord" ;;
-        *) echo "Invalid theme selection. Choose 1 for Everforest, 2 for Gruvbox, 3 for Tokyo Night, or 4 for Nord." && exit 1 ;;
+        "light") theme="light" ;;
+        "dark") theme="dark" ;;
+        *) echo "Invalid theme selection." && exit 1 ;;
     esac
 fi
 
 # Set variables based on the selected theme
 case $theme in
-    everforest)
-        background="#1d2021"
-        background_alt="#414b50"
-        foreground="#d3c6aa"
-        vscode_theme="Everforest Dark"
+    light)
+        gtk_theme="adw-gtk"
+        background="#ffffff"
+        foreground="#333333"
         ;;
-    gruvbox)
-        background="#282828"
-        background_alt="#504945"
-        foreground="#ebdbb2"
-        vscode_theme="Gruvbox Dark Medium"
-        ;;
-    tokyo)
-        background="#1a1b26"
-        background_alt="#3b4261"
-        foreground="#c0caf5"
-        vscode_theme="Tokyo Night Frameless"
-        ;;
-    nord)
-        background="#2e3440"
-        background_alt="#434c5e"
-        foreground="#d8dee9"
-        vscode_theme="Nord Deep"
+    dark)
+        gtk_theme="adw-gtk3-dark"
+        background="#1e1e1e"
+        foreground="#ffffff"
         ;;
 esac
 
@@ -49,8 +31,8 @@ esac
 xdotool search --name "Scratchpad" set_window --name "Multiplexer"
 
 # Polybar
-sed -i "s/themes\/.*\.ini/themes\/$theme\.ini/" ~/.config/polybar/config.ini
-~/.config/polybar/scripts/launch_polybar_program.sh
+sed -i "s/themes\/.*\.ini/themes\/$theme\.ini/" ~/.config/polybar/polybar.ini
+source ~/.config/polybar/scripts/launch_polybar_program.sh
 
 # i3
 sed -i "s/themes\/.*/themes\/$theme/" ~/.config/i3/config
@@ -60,19 +42,16 @@ i3 reload
 pkill dunst
 sed -i "s/background = \".*\"/background = \"$background\"/" ~/.config/dunst/dunstrc
 sed -i "s/foreground = \".*\"/foreground = \"$foreground\"/" ~/.config/dunst/dunstrc
-sed -i "s/frame_color = \".*\"/frame_color = \"$background_alt\"/" ~/.config/dunst/dunstrc
+
+# Gtk
+sed -i "s/Net\/ThemeName \".*\"/Net\/ThemeName \"$gtk_theme\"/" ~/.xsettingsd
+pkill -HUP xsettingsd
 
 # Alacritty
 sed -i "s/themes\/.*\.toml/themes\/$theme\.toml/" ~/.config/alacritty/alacritty.toml
 
 # Rofi
-sed -i "s/themes\/.*\.rasi/themes\/$theme\.rasi/" ~/.config/rofi/config.rasi
-
-# VSCode
-sed -i "s/\"workbench\.colorTheme\": \".*\"\s*/\"workbench.colorTheme\": \"$vscode_theme\"/" ~/.config/Code/User/settings.json
+sed -i "s/@theme \".*\"/@theme \"$theme\"/" ~/.config/rofi/rofi.rasi
 
 # Flameshot
-printf "[General]\nuiColor=%s\ncontrastUiColor=%s\n" "$background_alt" "$background" > ~/.config/flameshot/flameshot.ini
-
-# Wallpaper
-feh --bg-fill ~/Pictures/Wallpapers/$theme-wallpaper.png
+printf "[General]\nuiColor=%s\ncontrastUiColor=%s\n" "$background" "$foreground" > ~/.config/flameshot/flameshot.ini
