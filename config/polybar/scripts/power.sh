df702c112f805415a1b3dfc103dd4764b9c4264e
edu-flores <eduardo.floresramones@gmail.com>
2024-12-28 17:34:34 -0600

    smaller power menu

--- config/polybar/scripts/power.sh
+++ config/polybar/scripts/power.sh
@@ -5,7 +5,7 @@ options="⏼\n\n\n\n\n󰗽"
 
 # Display Rofi menu and store the selected option
 uptime=$(uptime -p | sed 's/up //')
-selected_option=$(echo -e "$options" | rofi -dmenu -config ~/.config/rofi/power.rasi -mesg "System Uptime: $uptime")
+selected_option=$(echo -e "$options" | rofi -dmenu -config ~/.config/rofi/power.rasi)
 
 # Perform actions based on the selected option
 case "$selected_option" in
